4c5ab75b3698a154a8d2e77d94f4ae10b5ff68a6
Kevin Denk <denk.kevin@web.de>
2017-10-18 15:09:52 +0200

    First draft for game board video

---
+++ test/CocktailMixer/Assets/Scripts/StreamVideo.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class StreamVideo : MonoBehaviour {
+
+	public RawImage image; 
+	public VideoClip videoClip; 
+
+	private VideoPlayer videoPlayer; 
+
+	// Use this for initialization
+	void Start () {
+		Application.runInBackground = true; 
+		StartCoroutine (playVideo()); 
+	}
+
+	void Update() {
+	}
+	
+	IEnumerator playVideo()
+	{
+		videoPlayer = gameObject.AddComponent<VideoPlayer> ();
+
+		videoPlayer.source = VideoSource.VideoClip; 
+		videoPlayer.clip = videoClip;
+
+		videoPlayer.playOnAwake = false; 
+		videoPlayer.audioOutputMode = VideoAudioOutputMode.None; 
+
+		videoPlayer.Prepare (); 
+
+		WaitForSeconds waitTime = new WaitForSeconds (1);
+		while (!videoPlayer.isPrepared) {
+			yield return waitTime; 
+			break; 
+		}
+
+		image.texture = videoPlayer.texture; 
+		while (videoPlayer.isPlaying) {
+			yield return null; 
+		}
+	}
+}
---
+++ test/CocktailMixer/Assets/Scripts/VideoPlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlayerController : MonoBehaviour {
+
+	public VideoPlayer player; 
+
+	// Use this for initialization
+	void Start () {
+
+		player.playOnAwake = false; 
+		player.frame = 100; 
+		player.isLooping = true; 
+
+		player.Play (); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
