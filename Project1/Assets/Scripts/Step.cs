66cf67a0bbe3133e3de2417138eb3b2514b519dc
Bastian Hoffmann <basti4242@googlemail.com>
2017-10-24 12:23:28 +0200

    implemented changes to recipes.json as discussed

--- Project1/Assets/Scripts/Step.cs
+++ Project1/Assets/Scripts/Step.cs
@@ -3,23 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public enum StepID
-{
-	cut,
-	crush,
-	shake,
-	muddle,
-	mix,
-	add,
-	shakeGently,
-	pour,
-	moveA,
-	moveB
-}
-
 [Serializable]
 public class Step {
-	public StepID id;
 	public List<string> highlightedObjects;
 	public List<string> instructions;
 	public bool	isWork;
