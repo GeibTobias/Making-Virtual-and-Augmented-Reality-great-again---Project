c772c3b00582a2a1c4ec1dbd1b2189cc07d49015
Bastian Hoffmann <basti4242@googlemail.com>
2017-10-29 19:40:45 +0100

    change cocktail image dynamically

--- Project1/Assets/Scripts/Bar.cs
+++ Project1/Assets/Scripts/Bar.cs
@@ -9,18 +9,19 @@ public class Bar : MonoBehaviour
 
 	public RecipeCollection collection;
 	public GameObject instructionsText;
-    public GameObject videoArea;
+	public GameObject videoArea;
 	public GameObject next;
 	public GameObject previous;
 	public GameObject exit;
 	public GameObject redo;
 	public GameObject done;
+	public GameObject cocktailImage;
 	private BarInitializer initializer;
 	private StateManager stateManager;
 	private KeywordDetector keywordDetector;
-    private KeyboardInput keyboardInput;
+	private KeyboardInput keyboardInput;
 
-    private bool keyboardInputEnabled = true; 
+	private bool keyboardInputEnabled = true;
 
 	// Use this for initialization
 	void Start ()
@@ -28,10 +29,10 @@ public class Bar : MonoBehaviour
 		stateManager = new StateManager ();
 		stateManager.StatusChanged += sm_OnStatusChanged;
 
-        keyboardInput = new KeyboardInput();
-        keyboardInput.CommandDetected += kd_OnCommandDetected;
+		keyboardInput = new KeyboardInput ();
+		keyboardInput.CommandDetected += kd_OnCommandDetected;
 
-        keywordDetector = this.GetComponent<KeywordDetector> ();
+		keywordDetector = this.GetComponent<KeywordDetector> ();
 		keywordDetector.CommandDetected += kd_OnCommandDetected;
 
 		collection = LoadRecipes ();
@@ -44,19 +45,18 @@ public class Bar : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-        if (keyboardInputEnabled && keyboardInput != null)
-        {
-            keyboardInput.isKeyPressed();
-        }
-    }
+		if (keyboardInputEnabled && keyboardInput != null) {
+			keyboardInput.isKeyPressed ();
+		}
+	}
 
 	private void setupUI ()
 	{
-        videoArea.GetComponent<VideoPlayer>().clip = null;
+		videoArea.GetComponent<VideoPlayer> ().clip = null;
 
 		updateLabels (stateManager.State);
 
-        switch (stateManager.State) {
+		switch (stateManager.State) {
 		case State.CocktailSelection:
 			instructionsText.GetComponents<TextMesh> () [0].text = "Say a Cocktail number:\n";
 			for (int i = 0; i < collection.recipes.Count; i++) {
@@ -75,51 +75,61 @@ public class Bar : MonoBehaviour
 			break;
 
 		case State.RecipeExecution:
-            stateManager.resetSteps(); 
+			stateManager.resetSteps ();
             // show first step
-            recipeNextCommand();
+			recipeNextCommand ();
 
-            break;
+			break;
 
 		case State.RecipeFinished:
-            dehighlightAllSpots();
-            stateManager.resetSteps();
+			dehighlightAllSpots ();
+			stateManager.resetSteps ();
 
-            instructionsText.GetComponents<TextMesh>()[0].text = "Enjoy the Cocktail.\nSay 'Redo' to do it again\nSay 'Done' to return to selection";
+			instructionsText.GetComponents<TextMesh> () [0].text = "Enjoy the Cocktail.\nSay 'Redo' to do it again\nSay 'Done' to return to selection";
 
-            break;
-		}	
+			break;
+		}
 	}
 
-	private void updateLabels(State state) {
+	private void updateLabels (State state)
+	{
 		switch (state) {
 		case State.CocktailSelection:
-			next.SetActive(false);
-			previous.SetActive(false);
-			exit.SetActive(false);
-			redo.SetActive(false);
-			done.SetActive(false);
+			next.SetActive (false);
+			previous.SetActive (false);
+			exit.SetActive (false);
+			redo.SetActive (false);
+			done.SetActive (false);
+			cocktailImage.SetActive (false);
 			break;
+
 		case State.BarInitialization:
-			next.SetActive(false);
-			previous.SetActive(false);
-			exit.SetActive(true);
-			redo.SetActive(false);
-			done.SetActive(true);
+			next.SetActive (false);
+			previous.SetActive (false);
+			exit.SetActive (true);
+			redo.SetActive (false);
+			done.SetActive (true);
+			cocktailImage.SetActive (true);
+			Texture2D texture = Resources.Load ("Images/" + stateManager.currentRecipe.picture) as Texture2D;
+			cocktailImage.GetComponent<Renderer> ().material.mainTexture = texture;
 			break;
+
 		case State.RecipeExecution:
-			next.SetActive(true);
-			previous.SetActive(stateManager.currentStepCount > 0);
-			exit.SetActive(true);
-			redo.SetActive(true);
-			done.SetActive(false);
+			next.SetActive (true);
+			previous.SetActive (stateManager.currentStepCount > 0);
+			exit.SetActive (true);
+			redo.SetActive (true);
+			done.SetActive (false);
+			cocktailImage.SetActive (true);
 			break;
+
 		case State.RecipeFinished:
-			next.SetActive(false);
-			previous.SetActive(false);
-			exit.SetActive(true);
-			redo.SetActive(true);
-			done.SetActive(true);
+			next.SetActive (false);
+			previous.SetActive (false);
+			exit.SetActive (true);
+			redo.SetActive (true);
+			done.SetActive (true);
+			cocktailImage.SetActive (true);
 			break;
 		}
 	}
@@ -142,222 +152,214 @@ public class Bar : MonoBehaviour
 
 		switch (stateManager.State) {
 		case State.CocktailSelection:
-			switch (e.Command) {
-			case SpeechCommand.One:
-				if (collection.recipes [0] != null) {
-					stateManager.currentRecipe = collection.recipes [0];
-					stateManager.State = State.BarInitialization;
-				}
-				break;
-			case SpeechCommand.Two:
-				if (collection.recipes [1] != null) {
-					stateManager.currentRecipe = collection.recipes [1];
-					stateManager.State = State.BarInitialization;
-				}
-				break;
-			case SpeechCommand.Three:
-				if (collection.recipes [2] != null) {
-					stateManager.currentRecipe = collection.recipes [2];
-					stateManager.State = State.BarInitialization;
-				}
-				break;
-			case SpeechCommand.Four:
-				if (collection.recipes [3] != null) {
-					stateManager.currentRecipe = collection.recipes [3];
-					stateManager.State = State.BarInitialization;
-				}
-				break;
-			case SpeechCommand.Five:
-				if (collection.recipes [4] != null) {
-					stateManager.currentRecipe = collection.recipes [4];
-					stateManager.State = State.BarInitialization;
-				}
-				break;
-			case SpeechCommand.Exit:
-				break;
-			}
-
+			this.cocktailSelection (e);
 			break;
 
 		case State.BarInitialization:
-                this.barinitialization(e);
-                break;
-		case State.RecipeExecution:
-                this.recipeExecution(e);
-                break; 
+			this.barinitialization (e);
+			break;
 
-        case State.RecipeFinished:
-                this.recipeFinished(e);
-                break;
+		case State.RecipeExecution:
+			this.recipeExecution (e);
+			break;
+
+		case State.RecipeFinished:
+			this.recipeFinished (e);
+			break;
 		}
 
 		updateLabels (stateManager.State);
 	}
 
-
-    private void barinitialization(CommandDetectedEventArgs e)
-    {
-        switch (e.Command)
-        {
-            case SpeechCommand.Done:
-                stateManager.State = State.RecipeExecution;
-                dehighlightAllSpots();
-				processStep(stateManager.currentStep);
-                break; 
-
-            case SpeechCommand.Exit:
-                dehighlightAllSpots();
-                stateManager.State = State.CocktailSelection;
-                break;
-        }
-    }
-
-
-    private void recipeFinished(CommandDetectedEventArgs e)
-    {
-        switch (e.Command)
-        {
-            case SpeechCommand.Done: case SpeechCommand.Exit:
-                dehighlightAllSpots();
-                stateManager.State = State.CocktailSelection;
-                
-                break;
-
-            case SpeechCommand.Repeat:
-                dehighlightAllSpots();
-                stateManager.resetSteps(); 
-                stateManager.State = State.BarInitialization;
-
-                break; 
-        }
-    }
-
-
-    private void recipeExecution(CommandDetectedEventArgs e)
-    {
-        switch (e.Command)
-        {
-            case SpeechCommand.Next:
-                recipeNextCommand();
-                break;
-
-            case SpeechCommand.Previous:
-                recipePrevCommand();
-                break;
-
-            case SpeechCommand.Repeat:
-                dehighlightAllSpots();
-                stateManager.resetSteps();
-                stateManager.State = State.BarInitialization;
-                break;
-
-            case SpeechCommand.Exit:
-                dehighlightAllSpots();
-                stateManager.State = State.CocktailSelection;
-                break;
-        }
-    }
+	private void cocktailSelection (CommandDetectedEventArgs e)
+	{
+		switch (e.Command) {
+		case SpeechCommand.One:
+			if (collection.recipes [0] != null) {
+				stateManager.currentRecipe = collection.recipes [0];
+				stateManager.State = State.BarInitialization;
+			}
+			break;
+		case SpeechCommand.Two:
+			if (collection.recipes [1] != null) {
+				stateManager.currentRecipe = collection.recipes [1];
+				stateManager.State = State.BarInitialization;
+			}
+			break;
+		case SpeechCommand.Three:
+			if (collection.recipes [2] != null) {
+				stateManager.currentRecipe = collection.recipes [2];
+				stateManager.State = State.BarInitialization;
+			}
+			break;
+		case SpeechCommand.Four:
+			if (collection.recipes [3] != null) {
+				stateManager.currentRecipe = collection.recipes [3];
+				stateManager.State = State.BarInitialization;
+			}
+			break;
+		case SpeechCommand.Five:
+			if (collection.recipes [4] != null) {
+				stateManager.currentRecipe = collection.recipes [4];
+				stateManager.State = State.BarInitialization;
+			}
+			break;
+		case SpeechCommand.Exit:
+			break;
+		}
+	}
 
 
-    private void recipeNextCommand()
-    {
-        Debug.Log("Process next command. "); 
-        if (stateManager.hasNextStep())
-        {
-            Step current = stateManager.next();
-            processStep(current);
-        }
-        else
-        {
-            Debug.Log("All steps executed. Recipe execution finished.");
-            stateManager.State = State.RecipeFinished;
-        }
-    }
+	private void barinitialization (CommandDetectedEventArgs e)
+	{
+		switch (e.Command) {
+		case SpeechCommand.Done:
+			stateManager.State = State.RecipeExecution;
+			dehighlightAllSpots ();
+			processStep (stateManager.currentStep);
+			break;
+
+		case SpeechCommand.Exit:
+			dehighlightAllSpots ();
+			stateManager.State = State.CocktailSelection;
+			break;
+		}
+	}
 
 
-    private void recipePrevCommand()
-    {
-        Debug.Log("Process prev command. ");
+	private void recipeFinished (CommandDetectedEventArgs e)
+	{
+		switch (e.Command) {
+		case SpeechCommand.Done:
+		case SpeechCommand.Exit:
+			dehighlightAllSpots ();
+			stateManager.State = State.CocktailSelection;
 
-        Step current = stateManager.previous(); 
-        processStep(current);
-    }
+			break;
 
+		case SpeechCommand.Repeat:
+			dehighlightAllSpots ();
+			stateManager.resetSteps ();
+			stateManager.State = State.BarInitialization;
 
-    private void processStep(Step current)
-    {
-        Debug.Log("Executing step");
-
-        // deactivate highlight for all spots
-        dehighlightAllSpots();
-
-        Dictionary<Ingredient, GameObject> dict = initializer.getIngGoMapping();
-
-        foreach (string item in current.ingredients)
-        {
-            Dictionary<Ingredient, GameObject> toHighlight = getEntryById(long.Parse( item ), dict);
-
-            foreach (KeyValuePair<Ingredient, GameObject> entry in toHighlight)
-            {
-                SpotHighlighter.activate(entry.Value, entry.Key); // can this be shifted into the other method?????
-            }
-        }
-
-        string text = "";
-        for (int i = 0; i < current.instructions.Count; i++)
-        {
-            text += (i + 1) + ") " + current.instructions[i] + "\n";
-        }
-        fillTextField(instructionsText.GetComponents<TextMesh>()[0], text);
-
-        videoArea.GetComponent<VideoPlayer>().clip = null;
-        videoArea.GetComponent<VideoPlayer>().clip = Resources.Load("Movies/" + current.video, typeof(VideoClip)) as VideoClip;
-    }
-
-    protected void fillTextField(TextMesh txt, string val)
-    {
-        float rowLimit = 3f; //find the sweet spot    
-
-        string[] parts = val.Split(' ');
-        string tmp = "";
-        txt.text = "";
-        for (int i = 0; i < parts.Length; i++)
-        {
-            tmp = txt.text;
-
-            txt.text += parts[i] + " ";
-            if (txt.GetComponent<Renderer>().bounds.extents.x > rowLimit)
-            {
-                tmp += Environment.NewLine;
-                tmp += parts[i] + " ";
-                txt.text = tmp;
-            }
-        }
-    }
+			break;
+		}
+	}
 
 
-    private Dictionary<Ingredient, GameObject> getEntryById(long id, Dictionary<Ingredient, GameObject> dict)
-    {
-        Dictionary<Ingredient, GameObject> result = new Dictionary<Ingredient, GameObject>(); 
+	private void recipeExecution (CommandDetectedEventArgs e)
+	{
+		switch (e.Command) {
+		case SpeechCommand.Next:
+			recipeNextCommand ();
+			break;
 
-        foreach (KeyValuePair<Ingredient, GameObject> entry in dict)
-        {
-            if( entry.Key.id == id )
-            {
-                result.Add(entry.Key, entry.Value); 
-            }
-        }
+		case SpeechCommand.Previous:
+			recipePrevCommand ();
+			break;
 
-        return result; 
-    }
+		case SpeechCommand.Repeat:
+			dehighlightAllSpots ();
+			stateManager.resetSteps ();
+			stateManager.State = State.BarInitialization;
+			break;
+
+		case SpeechCommand.Exit:
+			dehighlightAllSpots ();
+			stateManager.State = State.CocktailSelection;
+			break;
+		}
+	}
 
 
-    private void dehighlightAllSpots()
-    {
-        List<GameObject> tmp = initializer.getAllSpots(); 
-        foreach(GameObject go in tmp)
-        {
-            SpotHighlighter.deactive(go);
-        }
-    }
+	private void recipeNextCommand ()
+	{
+		Debug.Log ("Process next command. ");
+		if (stateManager.hasNextStep ()) {
+			Step current = stateManager.next ();
+			processStep (current);
+		} else {
+			Debug.Log ("All steps executed. Recipe execution finished.");
+			stateManager.State = State.RecipeFinished;
+		}
+	}
+
+
+	private void recipePrevCommand ()
+	{
+		Debug.Log ("Process prev command. ");
+
+		Step current = stateManager.previous ();
+		processStep (current);
+	}
+
+
+	private void processStep (Step current)
+	{
+		Debug.Log ("Executing step");
+
+		// deactivate highlight for all spots
+		dehighlightAllSpots ();
+
+		Dictionary<Ingredient, GameObject> dict = initializer.getIngGoMapping ();
+
+		foreach (string item in current.ingredients) {
+			Dictionary<Ingredient, GameObject> toHighlight = getEntryById (long.Parse (item), dict);
+
+			foreach (KeyValuePair<Ingredient, GameObject> entry in toHighlight) {
+				SpotHighlighter.activate (entry.Value, entry.Key); // can this be shifted into the other method?????
+			}
+		}
+
+		string text = "";
+		for (int i = 0; i < current.instructions.Count; i++) {
+			text += (i + 1) + ") " + current.instructions [i] + "\n";
+		}
+		fillTextField (instructionsText.GetComponents<TextMesh> () [0], text);
+
+		videoArea.GetComponent<VideoPlayer> ().clip = null;
+		videoArea.GetComponent<VideoPlayer> ().clip = Resources.Load ("Movies/" + current.video, typeof(VideoClip)) as VideoClip;
+	}
+
+	protected void fillTextField (TextMesh txt, string val)
+	{
+		float rowLimit = 3f; //find the sweet spot
+
+		string[] parts = val.Split (' ');
+		string tmp = "";
+		txt.text = "";
+		for (int i = 0; i < parts.Length; i++) {
+			tmp = txt.text;
+
+			txt.text += parts [i] + " ";
+			if (txt.GetComponent<Renderer> ().bounds.extents.x > rowLimit) {
+				tmp += Environment.NewLine;
+				tmp += parts [i] + " ";
+				txt.text = tmp;
+			}
+		}
+	}
+
+
+	private Dictionary<Ingredient, GameObject> getEntryById (long id, Dictionary<Ingredient, GameObject> dict)
+	{
+		Dictionary<Ingredient, GameObject> result = new Dictionary<Ingredient, GameObject> ();
+
+		foreach (KeyValuePair<Ingredient, GameObject> entry in dict) {
+			if (entry.Key.id == id) {
+				result.Add (entry.Key, entry.Value);
+			}
+		}
+
+		return result;
+	}
+
+
+	private void dehighlightAllSpots ()
+	{
+		List<GameObject> tmp = initializer.getAllSpots ();
+		foreach (GameObject go in tmp) {
+			SpotHighlighter.deactive (go);
+		}
+	}
 }
